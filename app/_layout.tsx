4cd28255bf3f4d06f68c5b9353f2ecf9946139ce
hieuhuhong29 <email@gmail.com>
2026-06-09 18:14:29 +0700

    simplify layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,4 @@
-// app/_layout.tsx
 import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-
-export default function RootLayout() {
-  return (
-    <>
-      <StatusBar style="light" backgroundColor="#0A0A0F" />
-      <Stack screenOptions={{ headerShown: false }} />
-    </>
-  );
+export default function Layout() {
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
